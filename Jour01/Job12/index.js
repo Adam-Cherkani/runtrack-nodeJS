57a9cced8fc6d78cabb28dfe41031385f6f906f7
Adam-Cherkani <adam.cherkani@laplateforme.io>
2024-05-13 15:15:52 +0200

    add jobs runtrack node.js

---
+++ Jour01/Job12/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Erreur du serveur');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Serveur démarré sur http://${hostname}:${port}/`);
+});
